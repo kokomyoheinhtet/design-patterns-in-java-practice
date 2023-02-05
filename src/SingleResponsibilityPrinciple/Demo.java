8c3d95d1e6f3f15245b8e7b5761c1fb2f1377a1f
Ko Ko Myo Hein Htet <koko.mhh@lmwn.com>
2023-02-05 12:03:16 +0700

    added srp

---
+++ src/SingleResponsibilityPrinciple/Demo.java
@@ -0,0 +1,16 @@
+package SingleResponsibilityPrinciple;
+
+
+public class Demo {
+
+    public static void main(String[] args) throws Exception {
+        Journal journal = new Journal();
+        journal.addEntries("I ate wat thar dote htoe yesterday");
+        journal.addEntries("I have no idea what to eat today");
+        System.out.println(journal);
+
+        Persistence persistence = new Persistence();
+        String filename = "./firstJ.txt";
+        persistence.saveToFile(journal, filename, true);
+    }
+}
---
+++ src/SingleResponsibilityPrinciple/Persistence.java
@@ -0,0 +1,26 @@
+package SingleResponsibilityPrinciple;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.net.URL;
+
+/*
+handles the responsibility of persisting objects
+ */
+public class Persistence {
+    public void saveToFile(Journal journal, String filename, boolean overwrite) throws FileNotFoundException {
+        if (overwrite || new File(filename).exists()) {
+            try (PrintStream out = new PrintStream(filename)) {
+                out.println(journal.toString());
+            }
+        }
+    }
+
+    public void load(Journal journal, String filename) {
+    }
+
+    public void load(Journal journal, URL url) {
+    }
+
+}
