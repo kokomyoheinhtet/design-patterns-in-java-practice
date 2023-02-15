c58f0cf0b3aaad2ff494e567af7d9859e16b52c4
Ko Ko Myo Hein Htet <koko.mhh@lmwn.com>
2023-02-15 15:04:13 +0700

    added FacetedBuilderDemo

---
+++ src/A1_Builder/FacetedBuilderDemo.java
@@ -0,0 +1,98 @@
+package A1_Builder;
+
+public class FacetedBuilderDemo {
+}
+
+class Person {
+    public String streetAddress, postcode, city;
+
+    public String companyName, position;
+    public int annualIncome;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", city='" + city + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", position='" + position + '\'' +
+                ", annualIncome=" + annualIncome +
+                '}';
+    }
+}
+
+class PersonBuilder {
+    protected Person person = new Person();
+
+    public PersonAddressBuilder lives() {
+        return new PersonAddressBuilder(person);
+    }
+
+    public PersonJobBuilder works() {
+        return new PersonJobBuilder(person);
+    }
+
+    public Person build() {
+        return person;
+    }
+}
+
+class PersonAddressBuilder extends PersonBuilder {
+    public PersonAddressBuilder(Person person) {
+        this.person = person;
+    }
+
+    public PersonAddressBuilder at(String streetAddress) {
+        person.streetAddress = streetAddress;
+        return this;
+    }
+
+    public PersonAddressBuilder withPostcode(String postcode) {
+        person.postcode = postcode;
+        return this;
+    }
+
+    public PersonAddressBuilder in(String city) {
+        person.city = city;
+        return this;
+    }
+}
+
+class PersonJobBuilder extends PersonBuilder {
+    public PersonJobBuilder(Person person) {
+        this.person = person;
+    }
+
+    public PersonJobBuilder at(String companyName) {
+        person.companyName = companyName;
+        return this;
+    }
+
+    public PersonJobBuilder asA(String position) {
+        person.position = position;
+        return this;
+    }
+
+    public PersonJobBuilder earning(int annualIncome) {
+        person.annualIncome = annualIncome;
+        return this;
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        PersonBuilder pb = new PersonBuilder();
+        Person person = pb
+                .lives()
+                .at("Makkasan")
+                .in("Bangkok")
+                .withPostcode("10400")
+                .works()
+                .at("my company")
+                .asA("Software Engineer")
+                .earning(30000)
+                .build();
+        System.out.println(person);
+    }
+}
